e095b8ef80ce5fd639345aa7523fa9d9f7877d17
Alan Bi <alan.bi326@gmail.com>
2020-11-07 23:46:08 -0800

    Initial commit

---
+++ src/test/java/com/rocketden/tester/CompilerTests.java
@@ -0,0 +1,16 @@
+package com.rocketden.tester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class CompilerTests {
+
+	@Test
+	public void contextLoads() {
+		assertTrue(true);
+	}
+
+}
