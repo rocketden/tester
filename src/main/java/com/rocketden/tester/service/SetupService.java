78329ed5bd27d41dcdc1ef1f5134f3f17f61eaf7
Alan Bi <alan.bi326@gmail.com>
2020-11-08 22:11:25 -0800

    Implement basic setup service

--- src/main/java/com/rocketden/tester/service/SetupService.java
+++ src/main/java/com/rocketden/tester/service/SetupService.java
@@ -3,10 +3,36 @@ package com.rocketden.tester.service;
 import com.rocketden.tester.dto.RunRequest;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 @Service
 public class SetupService {
 
     public String createTempFolder(RunRequest request) {
-        return "folder_name";
+        String folder = generateRandomFolder();
+        boolean success = new File(folder).mkdirs();
+
+        if (success) {
+            String code = request.getCode();
+            String driverFile = String.format("%s/run.sh", folder);
+
+            try {
+                // Create a file called run.sh with the contents of the code variable
+                Files.write(Paths.get(driverFile), code.getBytes(StandardCharsets.UTF_8));
+            } catch (IOException e) {
+                // Error handling
+                System.out.println("An error occurred");
+            }
+        }
+
+        return folder;
+    }
+
+    private String generateRandomFolder() {
+        return "folder12";
     }
 }
