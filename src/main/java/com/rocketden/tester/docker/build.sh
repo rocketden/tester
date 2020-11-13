8815989b8bb2d6ec13f06f5b51f815c81e40aeef
Alan Bi <alan.bi326@gmail.com>
2020-11-12 22:12:52 -0800

    Refactor files and fix issues

---
+++ src/main/java/com/rocketden/tester/docker/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Run this command to build the Docker image used in the app
+
+docker build --tag 'rocketden/tester' ../docker
--- src/main/java/com/rocketden/tester/service/DockerService.java
+++ src/main/java/com/rocketden/tester/service/DockerService.java
@@ -9,14 +9,14 @@ public class DockerService {
 
     public String spawnAndRun(String folder) {
         try {
-            // First ensure that docker image is built: docker build --tag 'tester' ../docker
+            // First ensure that docker image is built: docker build --tag 'rocketden/tester' ../docker
             // TODO: figure out how to do this
 
             // Mount folder to docker container
             String mount = String.format("$PWD/../temp/%s:/app", folder);
 
             // Run docker container with copied folder, and delete container afterwards
-            String[] runCommands = {"docker", "run", "-v", mount, "-t", "tester", "--rm"};
+            String[] runCommands = {"docker", "run", "-v", mount, "-t", "rocketden/tester", "--rm"};
             Runtime.getRuntime().exec(runCommands);
 
             // TODO: check for output file with results
