514a8d93af70d6fb7ec98e76026bc37b95eac1e2
Alan Bi <alan.bi326@gmail.com>
2020-11-12 22:41:45 -0800

    Get basic docker system working

--- src/main/java/com/rocketden/tester/docker/build.sh
+++ src/main/java/com/rocketden/tester/docker/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 # Run this command to build the Docker image used in the app
-
 docker build --tag 'rocketden/tester' ../docker
+
+# Remove dangling images that are no longer needed
+docker rmi `docker images -f "dangling=true" -q`
--- src/main/java/com/rocketden/tester/docker/entrypoint.sh
+++ src/main/java/com/rocketden/tester/docker/entrypoint.sh
@@ -4,5 +4,5 @@
 # Copies given folder contents from the temp directory into docker container and runs the driver script
 # Example usage: ./entrypoint.sh folder123
 
-cp -a ../temp/$1/. .
-bash ./run.sh
+#cp -a ../temp/$1/. .
+bash ./code/run.sh
