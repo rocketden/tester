1e3b0b9c4b59fe6ad1f85738ab61e708cfd13ad9
Alan Bi <alan.bi326@gmail.com>
2021-01-04 10:52:24 -0800

    Move manual scripts to separate folder

--- src/main/java/com/rocketden/tester/scripts/build.sh
+++ src/main/java/com/rocketden/tester/scripts/build.sh
@@ -3,5 +3,3 @@
 # Run this command to build the Docker image used in the app
 docker build --tag 'rocketden/tester' ../docker
 
-# Remove dangling images that are no longer needed
-docker rmi `docker images -f "dangling=true" -q`
