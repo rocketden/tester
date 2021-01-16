7cbde81e3778f95754887d8882d62501891083ae
Jerry Ye <jerry1ye10@gmail.com>
2021-01-16 11:58:03 -0500

    make small pr fixes

--- src/main/java/com/rocketden/tester/dto/RunRequest.java
+++ src/main/java/com/rocketden/tester/dto/RunRequest.java
@@ -3,17 +3,9 @@ package com.rocketden.tester.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-//@Getter
-//@Setter
+@Getter
+@Setter
 public class RunRequest {
     private String code;
     private String language; 
-
-    public String getCode(){
-        return code;
-    }
-
-    public String getLanguage(){
-        return language;
-    }
 }
