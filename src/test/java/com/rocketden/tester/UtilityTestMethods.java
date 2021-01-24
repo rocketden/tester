073ac11dc76dda584862f462f7639b5778f5bd4f
Alan Bi <alan.bi326@gmail.com>
2021-01-24 01:28:31 -0800

    Create first test for repo

---
+++ src/test/java/com/rocketden/tester/UtilityTestMethods.java
@@ -0,0 +1,18 @@
+package com.rocketden.tester;
+
+import java.lang.reflect.Type;
+
+import com.google.gson.Gson;
+
+public class UtilityTestMethods {
+
+    public static String convertObjectToJsonString(Object o) {
+        Gson gson = new Gson();
+        return gson.toJson(o);
+    }
+
+    public static <T> T toObject(String json, Class<T> c) {
+        Gson gson = new Gson();
+        return gson.fromJson(json, c);
+    }
+}
