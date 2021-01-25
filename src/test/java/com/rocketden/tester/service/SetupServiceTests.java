a0b018fc720574b6c2d45c82624a3bdfa5a3adda
Alan Bi <alan.bi326@gmail.com>
2021-01-24 22:25:05 -0800

    Finish implementing setup service tests

--- src/test/java/com/rocketden/tester/service/SetupServiceTests.java
+++ src/test/java/com/rocketden/tester/service/SetupServiceTests.java
@@ -1,24 +1,34 @@
 package com.rocketden.tester.service;
 
 import com.rocketden.tester.dto.RunRequest;
+import com.rocketden.tester.exception.DockerSetupError;
+import com.rocketden.tester.exception.api.ApiException;
 import com.rocketden.tester.model.Language;
 import com.rocketden.tester.model.problem.Problem;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.io.File;
+import java.nio.file.Paths;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 public class SetupServiceTests {
 
     @Mock
-    List<DriverGeneratorService> test;
+    DriverFileService driverFileService;
 
     @Spy
     @InjectMocks
@@ -32,11 +42,16 @@ public class SetupServiceTests {
         RunRequest request = new RunRequest();
         request.setCode(CODE);
         request.setLanguage(LANGUAGE);
-        request.setProblem(new Problem());
+        Problem problem = new Problem();
+        request.setProblem(problem);
 
         String folder = setupService.createTempFolder(request);
 
-        // TODO: assert two files are created
+        String driverFile = String.format("%s/Driver.%s", folder, LANGUAGE.getExtension());
+        String solutionFile = String.format("%s/Solution.%s", folder, LANGUAGE.getExtension());
+
+        verify(driverFileService).writeDriverFile(eq(driverFile), eq(LANGUAGE), eq(problem));
+        assertTrue(new File(solutionFile).exists());
 
         assertTrue(folder.contains("src/main/java/com/rocketden/tester/temp"));
 
@@ -46,11 +61,29 @@ public class SetupServiceTests {
 
     @Test
     public void deleteTempFolderSuccess() {
+        RunRequest request = new RunRequest();
+        request.setCode(CODE);
+        request.setLanguage(LANGUAGE);
+        Problem problem = new Problem();
+        request.setProblem(problem);
 
+        String folder = setupService.createTempFolder(request);
+
+        assertTrue(new File(folder).exists());
+
+        setupService.deleteTempFolder(folder);
+
+        assertFalse(new File(folder).exists());
     }
 
     @Test
     public void deleteTempFolderInvalidFolder() {
+        String pwd = Paths.get("").toAbsolutePath().toString();
+        String relativePath = "src/main/java/com/rocketden/tester/nontempfolder";
 
+        String safeTestDeletePath = String.format("%s/%s", pwd, relativePath);
+        ApiException exception = assertThrows(ApiException.class, () -> setupService.deleteTempFolder(safeTestDeletePath));
+
+        assertEquals(DockerSetupError.DELETE_TEMP_FOLDER, exception.getError());
     }
 }
