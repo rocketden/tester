01854a6b90278d20bf5b2c94bf05e762a7ec813f
Alan Bi <alan.bi326@gmail.com>
2021-01-24 20:50:33 -0800

    Implement run fail test with temp folder cleanup ensured

--- src/test/java/com/rocketden/tester/service/RunnerServiceTests.java
+++ src/test/java/com/rocketden/tester/service/RunnerServiceTests.java
@@ -1,6 +1,9 @@
 package com.rocketden.tester.service;
 
 import com.rocketden.tester.dto.RunRequest;
+import com.rocketden.tester.exception.DockerSetupError;
+import com.rocketden.tester.exception.api.ApiError;
+import com.rocketden.tester.exception.api.ApiException;
 import com.rocketden.tester.model.Language;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,6 +13,8 @@ import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -33,17 +38,27 @@ public class RunnerServiceTests {
         RunRequest request = new RunRequest();
         request.setLanguage(LANGUAGE);
 
+        Mockito.doReturn(TEMP_FOLDER).when(setupService).createTempFolder(request);
         runnerService.run(request);
 
-        Mockito.doReturn(TEMP_FOLDER).when(setupService).createTempFolder(request);
         verify(dockerService).spawnAndRun(TEMP_FOLDER, request.getLanguage());
-
         verify(setupService).deleteTempFolder(TEMP_FOLDER);
     }
 
     @Test
     public void runFails() {
-        // 1. Returns the given ApiError
-        // 2. deleteTempFolder is still called if an error occurs
+        RunRequest request = new RunRequest();
+        request.setLanguage(LANGUAGE);
+
+        ApiError ERROR = DockerSetupError.BUILD_DOCKER_CONTAINER;
+
+        Mockito.doReturn(TEMP_FOLDER).when(setupService).createTempFolder(request);
+        Mockito.doThrow(new ApiException(ERROR)).when(dockerService).spawnAndRun(Mockito.anyString(), Mockito.any());
+
+        ApiException exception = assertThrows(ApiException.class, () -> runnerService.run(request));
+
+        assertEquals(ERROR, exception.getError());
+        // Despite the exception, the code cleanup step still occurs
+        verify(setupService).deleteTempFolder(TEMP_FOLDER);
     }
 }
