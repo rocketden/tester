992fd2285c20bb4bbf6f8bac05ce47b1020df7ac
Alan Bi <alan.bi326@gmail.com>
2021-01-24 22:03:56 -0800

    Write createTempFolder tests pending merge

--- src/test/java/com/rocketden/tester/service/SetupServiceTests.java
+++ src/test/java/com/rocketden/tester/service/SetupServiceTests.java
@@ -1,22 +1,47 @@
 package com.rocketden.tester.service;
 
+import com.rocketden.tester.dto.RunRequest;
+import com.rocketden.tester.model.Language;
+import com.rocketden.tester.model.problem.Problem;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @ExtendWith(MockitoExtension.class)
 public class SetupServiceTests {
+
+    @Mock
+    List<DriverGeneratorService> test;
 
     @Spy
     @InjectMocks
     private SetupService setupService;
 
+    private static final String CODE = "print('hi')";
+    private static final Language LANGUAGE = Language.PYTHON;
+
     @Test
     public void createTempFolderSuccess() {
-        // Driver and Solution files are supposedly created (mocked)
-        // Returns correct folder path
+        RunRequest request = new RunRequest();
+        request.setCode(CODE);
+        request.setLanguage(LANGUAGE);
+        request.setProblem(new Problem());
+
+        String folder = setupService.createTempFolder(request);
+
+        // TODO: assert two files are created
+
+        assertTrue(folder.contains("src/main/java/com/rocketden/tester/temp"));
+
+        // Cleanup any files created (note: this will not always be reached if failures occur)
+        setupService.deleteTempFolder(folder);
     }
 
     @Test
