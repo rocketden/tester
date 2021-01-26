c68794168e5bbe05470b2de162e1efec533fdb77
Alan Bi <alan.bi326@gmail.com>
2021-01-26 00:41:26 -0800

    Create parser test files and seven worthy test stubs

---
+++ src/test/java/com/rocketden/tester/service/parsers/InputParserTests.java
@@ -0,0 +1,50 @@
+package com.rocketden.tester.service.parsers;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class InputParserTests {
+
+    @Spy
+    @InjectMocks
+    private InputParser inputParser;
+
+    @Test
+    public void parseDouble() {
+
+    }
+
+    @Test
+    public void parseIntArray() {
+
+    }
+
+    @Test
+    public void parseBooleanArray() {
+
+    }
+
+    @Test
+    public void parseMultipleArguments() {
+
+    }
+
+    @Test
+    public void parseFailsNotInvalidFormat() {
+
+    }
+
+    @Test
+    public void parseFailsTypeMismatch() {
+
+    }
+
+    @Test
+    public void parseFailsArrayTypeMismatch() {
+        
+    }
+}
