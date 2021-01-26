0a73a2de6d837a947f042b8a554d8bcadcad5c62
Chris Elliott <elliott77chris@gmail.com>
2021-01-26 14:40:26 -0800

    Update methodName field to be a language->string map.

--- src/main/java/com/rocketden/tester/model/problem/Problem.java
+++ src/main/java/com/rocketden/tester/model/problem/Problem.java
@@ -3,6 +3,8 @@ package com.rocketden.tester.model.problem;
 import java.util.List;
 import java.util.Map;
 
+import com.rocketden.tester.model.Language;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,7 +18,7 @@ public class Problem {
     private List<ProblemTestCase> testCases;
 
     // Additional fields for the creation of the driver file.
-    private String methodName;
+    private Map<Language, String> methodNames;
     private Map<String, ProblemIOType> inputNameTypeMap;
     private ProblemIOType outputType;
 }
