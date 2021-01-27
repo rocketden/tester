d5ce75a99dddb916ba1063b9a1c0338aa0e02e96
Alan Bi <alan.bi326@gmail.com>
2021-01-26 21:55:29 -0800

    Get Gson to parse into correct types

--- src/main/java/com/rocketden/tester/exception/ParserError.java
+++ src/main/java/com/rocketden/tester/exception/ParserError.java
@@ -9,8 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum ParserError implements ApiError {
 
     INCORRECT_COUNT(HttpStatus.BAD_REQUEST, "Please specify the correct number of inputs for ths problem."),
-    INVALID_FORMAT(HttpStatus.BAD_REQUEST, "Please enter each line of your input in a valid format."),
-    INVALID_TYPE(HttpStatus.BAD_REQUEST, "The type you provided does not match the specified type for this problem.");
+    INVALID_INPUT(HttpStatus.BAD_REQUEST, "Please ensure each line of input is valid and is of the correct type.");
 
     private final HttpStatus status;
     private final ApiErrorResponse response;
--- src/main/java/com/rocketden/tester/service/parsers/InputParser.java
+++ src/main/java/com/rocketden/tester/service/parsers/InputParser.java
@@ -1,7 +1,9 @@
 package com.rocketden.tester.service.parsers;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 import com.rocketden.tester.exception.ParserError;
+import com.rocketden.tester.exception.ProblemError;
 import com.rocketden.tester.exception.api.ApiException;
 import com.rocketden.tester.model.problem.Problem;
 import com.rocketden.tester.model.problem.ProblemIOType;
@@ -20,24 +22,35 @@ public class InputParser {
     private final Gson gson = new Gson();
 
     public List<Object> parseTestCase(Problem problem, ProblemTestCase testCase) {
+        List<ProblemInput> problemInputs = problem.getProblemInputs();
         String[] rawInputs = splitTestCaseIntoInputs(testCase.getInput());
 
-        if (rawInputs.length != problem.getProblemInputs().size()) {
+        if (rawInputs.length != problemInputs.size()) {
             throw new ApiException(ParserError.INCORRECT_COUNT);
         }
 
         List<Object> inputs = new ArrayList<>();
         for (int i = 0; i < rawInputs.length; i++) {
-            Object object = gson.fromJson(rawInputs[i], Object.class);
+            ProblemIOType type = problemInputs.get(i).getType();
+            Object object = parseRawInputOfGivenType(rawInputs[i], type.getClassType());
+
             inputs.add(object);
         }
 
         return inputs;
     }
 
+    private Object parseRawInputOfGivenType(String input, Class<?> classType) {
+        try {
+            return gson.fromJson(input, classType);
+        } catch (JsonSyntaxException e) {
+            throw new ApiException(ParserError.INVALID_INPUT);
+        }
+    }
+
     protected String[] splitTestCaseIntoInputs(String input) {
         if (input == null) {
-            throw new ApiException(ParserError.INVALID_FORMAT);
+            throw new ApiException(ParserError.INVALID_INPUT);
         }
 
         return input.split("\n");
@@ -47,10 +60,10 @@ public class InputParser {
         InputParser parser = new InputParser();
 
         ProblemTestCase testCase = new ProblemTestCase();
-        testCase.setInput("[1, 2, 3]");
+        testCase.setInput("4");
 
         ProblemInput problemInput = new ProblemInput();
-        problemInput.setType(ProblemIOType.ARRAY_INTEGER);
+        problemInput.setType(ProblemIOType.INTEGER);
 
         Problem problem = new Problem();
         problem.setTestCases(Collections.singletonList(testCase));
