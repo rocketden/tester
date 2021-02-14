ebbdd1bc32f75b709b3a6e4b5954404892fed1db
Chris Elliott <elliott77chris@gmail.com>
2021-02-13 20:09:32 -0800

    Complete the isOutputCorrect function by converting results to objects.

--- src/main/java/com/rocketden/tester/service/parsers/OutputParser.java
+++ src/main/java/com/rocketden/tester/service/parsers/OutputParser.java
@@ -6,18 +6,23 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 import com.rocketden.tester.dto.ResultDto;
 import com.rocketden.tester.dto.RunDto;
 import com.rocketden.tester.exception.ParserError;
 import com.rocketden.tester.exception.api.ApiException;
 import com.rocketden.tester.model.OutputSection;
 import com.rocketden.tester.model.problem.Problem;
+import com.rocketden.tester.model.problem.ProblemIOType;
 import com.rocketden.tester.model.problem.ProblemTestCase;
 
 import org.springframework.stereotype.Service;
 
 @Service
 public class OutputParser {
+
+    private final Gson gson = new Gson();
 
     public static final String DELIMITER_TEST_CASE = "###########_TEST_CASE_############";
     public static final String DELIMITER_SUCCESS = "###########_SUCCESS_############";
@@ -49,7 +54,7 @@ public class OutputParser {
             if (s.equals(DELIMITER_TEST_CASE)) {
                 parseTestCaseOutput(outputSection,
                     output.toString(), results, result,
-                    testCases.get(results.size()));
+                    testCases.get(results.size()), problem.getOutputType());
                 result = new ResultDto();
                 output.setLength(0);
                 outputSection = OutputSection.TEST_CASE;
@@ -80,7 +85,7 @@ public class OutputParser {
         // Add the last remaining test case output, if one exists.
         parseTestCaseOutput(outputSection,
             output.toString(), results, result,
-            testCases.get(results.size()));
+            testCases.get(results.size()), problem.getOutputType());
 
         // Throw error if more tests were captured than exist.
         if (results.size() != testCases.size()) {
@@ -113,7 +118,7 @@ public class OutputParser {
      */
     private void parseTestCaseOutput(OutputSection outputSection,
         String outputStr, List<ResultDto> results, ResultDto result,
-        ProblemTestCase testCase) {
+        ProblemTestCase testCase, ProblemIOType outputType) {
         // Update the result as expected or throw error if misformatted.
         if (outputSection == OutputSection.TEST_CASE) {
             throw new ApiException(ParserError.MISFORMATTED_OUTPUT);
@@ -122,7 +127,7 @@ public class OutputParser {
             result.setUserOutput(outputStr);
             result.setError(null);
             result.setCorrectOutput(testCase.getOutput());
-            result.setCorrect(isOutputCorrect(outputStr, testCase));
+            result.setCorrect(isOutputCorrect(outputStr, testCase, outputType));
             results.add(result);
         } else if (outputSection == OutputSection.FAILURE) {
             // Set the result fields.
@@ -151,7 +156,18 @@ public class OutputParser {
     }
 
     // Return whether the user's output is correct.
-    private boolean isOutputCorrect(String output, ProblemTestCase testCase) {
-        return output.equals(testCase.getOutput());
+    private boolean isOutputCorrect(String outputStr, ProblemTestCase testCase, ProblemIOType outputType) {
+        Object userOutput = parseRawOutputOfGivenType(outputStr, outputType.getClassType());
+        Object correctOutput = parseRawOutputOfGivenType(testCase.getOutput(), outputType.getClassType());
+        return userOutput.equals(correctOutput);
+    }
+
+    // Helper function to parse the output into the given object.
+    private Object parseRawOutputOfGivenType(String output, Class<?> classType) {
+        try {
+            return gson.fromJson(output, classType);
+        } catch (JsonSyntaxException e) {
+            throw new ApiException(ParserError.INVALID_OUTPUT);
+        }
     }
 }
